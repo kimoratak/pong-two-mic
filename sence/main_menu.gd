af7faf9702ab8b7ca53fb92dae86d47dde2531d6
kimoratak <127212138+kimoratak@users.noreply.github.com>
2025-09-11 13:56:01 +0700

    update game

---
+++ sence/main_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://sence/level_select_menu.tscn")
+	print("start game")
+	pass # Replace with function body.
+
+
+func _on_setting_pressed() -> void:
+	print("setting")
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	print("exit")
+	pass # Replace with function body.
